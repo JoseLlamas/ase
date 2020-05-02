ba06c7639cba212d6ce370883ea365e757ab5ec9
Johann Hermine <magico.fairy.tale@gmail.com>
2020-05-02 14:30:51 -0500

    se agrega la libreria djangorestframework y la imagen del diagrama entidad_relacion (no terminada)

--- app/settings.py
+++ app/settings.py
@@ -30,14 +30,15 @@ ENV = os.environ.get('ENV', 'development')
 DEBUG = ENV == 'development'
 
 ALLOWED_HOSTS = [
-    '0.0.0.0'
+    '0.0.0.0',
+    'localhost'
 ]
 
 # Application definition
 
 INSTALLED_APPS = [
     #'django.contrib.admin',
-    #'django.contrib.auth',
+    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    #'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
